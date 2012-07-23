62b42e6c7d282be5e6023c8d4151b4d07fd494a3
Ketoth Xupack <ketoth.xupack@gmail.com>
2012-07-23 15:13:28 +0700

    naive isStarted method for AsyncApp
    Signed-off-by: Ketoth Xupack <ketoth.xupack@gmail.com>

--- projects/apptools/src/main/java/org/nohope/app/AsyncApp.java
+++ projects/apptools/src/main/java/org/nohope/app/AsyncApp.java
@@ -1,12 +1,14 @@
 package org.nohope.app;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 /**
  * @author <a href="mailto:ketoth.xupack@gmail.com">ketoth xupack</a>
  * @since 7/15/12 3:23 PM
  */
 public abstract class AsyncApp extends App {
+    private final AtomicBoolean started = new AtomicBoolean(false);
     private final CountDownLatch latch;
 
     protected abstract void onStop();
@@ -21,18 +23,25 @@ public abstract class AsyncApp extends App {
 
     public final void stop() {
         onStop();
+        started.set(false);
         latch.countDown();
     }
 
     @Override
     public final void start() throws InterruptedException {
         super.start();
+        started.set(true);
         latch.await();
     }
 
     @Override
     protected final void onVMShutdownWrapper() {
         super.onVMShutdownWrapper();
+        started.set(false);
         latch.countDown();
+    }
+
+    public final boolean isStarted() {
+        return started.get();
     }
 }
