80c683b50b133aecfb7dcf2f43ed70f37be2fd39
Ketoth Xupack <ketoth.xupack@gmail.com>
2012-08-02 12:41:17 +0700

    inspections & style fixes
    Signed-off-by: Ketoth Xupack <ketoth.xupack@gmail.com>

--- projects/slf4j-logging/src/test/java/org/nohope/logging/EnhancedLoggerTest.java
+++ projects/slf4j-logging/src/test/java/org/nohope/logging/EnhancedLoggerTest.java
@@ -14,23 +14,23 @@ public final class EnhancedLoggerTest {
 
     @Test
     public void debug() {
-        org.slf4j.Logger logger = createMock(org.slf4j.Logger.class);
+        final org.slf4j.Logger logger = createMock(org.slf4j.Logger.class);
 
-        Capture<Object[]> argsC = new Capture<Object[]>();
-        Capture<String> formatC = new Capture<String>();
+        final Capture<Object[]> argsC = new Capture<>();
+        final Capture<String> formatC = new Capture<>();
 
         logger.debug(capture(formatC), capture(argsC));
         expectLastCall();
         replay(logger);
 
-        EnhancedLogger elogger = new EnhancedLogger(logger);
+        final EnhancedLogger elogger = new EnhancedLogger(logger);
 
         elogger.debug("%s %s %s %s %s", 1, 2, 3, 4, 5);
         assertEquals("%s %s %s %s %s", formatC.getValue());
 
         verify(logger);
 
-        Logger logger1 = LoggerFactory.getLogger(EnhancedLoggerTest.class);
+        final Logger logger1 = LoggerFactory.getLogger(EnhancedLoggerTest.class);
         logger1.warn(new IllegalStateException(), "{} {}", 1, 2);
 
     }
