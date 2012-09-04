ed4428bfcb6344f4cd090969b1e9dc3d89af0a31
Fei Wong Reed <feiwreed@gmail.com>
2012-09-04 14:42:00 +0700

    imports

--- projects/typetools/src/test/java/org/nohope/typetools/StringUtilsTest.java
+++ projects/typetools/src/test/java/org/nohope/typetools/StringUtilsTest.java
@@ -3,11 +3,7 @@ package org.nohope.typetools;
 import org.junit.Test;
 import org.nohope.reflection.UtilitiesTestSupport;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNull;
