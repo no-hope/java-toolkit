eab193a212f1288f63fa9c6dc8dd7733959073fe
Ketoth Xupack <ketoth.xupack@gmail.com>
2012-09-18 17:36:37 +0700

    CollectionUtils introduced
    Signed-off-by: Ketoth Xupack <ketoth.xupack@gmail.com>

---
+++ projects/typetools/src/main/java/org/nohope/typetools/collection/CollectionUtils.java
@@ -0,0 +1,20 @@
+package org.nohope.typetools.collection;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author <a href="mailto:ketoth.xupack@gmail.com">ketoth xupack</a>
+ * @since 9/18/12 5:32 PM
+ */
+public class CollectionUtils {
+    private CollectionUtils() {
+    }
+
+    public static<T> List<T> fillList(final int count, final T placeholder) {
+        final List<T> placeholders = new ArrayList<>(count);
+        Collections.fill(placeholders, placeholder);
+        return placeholders;
+    }
+}
