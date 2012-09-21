78872387539c6f71d9eeb2702a18e347c47415ea
Fei Wong Reed <feiwreed@gmail.com>
2012-09-21 14:45:46 +0700

    json helpers refined

--- projects/typetools/src/main/java/org/nohope/typetools/JSON.java
+++ projects/typetools/src/main/java/org/nohope/typetools/JSON.java
@@ -34,25 +34,43 @@ public final class JSON {
         usualMapper.setSerializationInclusion(NON_EMPTY);
     }
 
-    public static String jsonifyPretty(final Object obj,
-                                       final String onErrorMessage) {
-        try {
-            return prettyMapper.writeValueAsString(obj);
-        } catch (IOException e) {
-            LOG.error(e, "Unable to jsonify object of class {}",
-                    obj == null ? null : obj.getClass().getCanonicalName());
-            return onErrorMessage;
-        }
+    public static String pretty(final Object obj) {
+        return pretty(obj, defaultErrorMessage(obj));
     }
 
-    public static String jsonifyPretty(final Object obj) {
-        return jsonifyPretty(obj, defaultErrorMessage(obj));
+    public static String jsonify(final Object obj) {
+        return jsonify(obj, defaultErrorMessage(obj));
     }
 
+    /**
+     * Serializes given object with Jackson, then deserializes into class required.
+     * Not so fast, so shouldn't be used for deep copying.
+     *
+     * @param source
+     * @param clazz
+     * @param <T>
+     * @return
+     * @throws IOException
+     */
+    public static <T> T copyAs(final Object source, final Class<T> clazz) throws IOException {
+        return usualMapper.readValue(usualMapper.writeValueAsBytes(source), clazz);
+    }
+
+
+    // TODO: make this private after legacy eradication
     public static String jsonify(final Object obj,
                                  final String onErrorMessage) {
+        return jsonifyWith(usualMapper, obj, onErrorMessage);
+    }
+
+    private static String pretty(final Object obj,
+                         final String onErrorMessage) {
+        return jsonifyWith(prettyMapper, obj, onErrorMessage);
+    }
+
+    private static String jsonifyWith(final ObjectMapper mapper, final Object obj, final String onErrorMessage) {
         try {
-            return usualMapper.writeValueAsString(obj);
+            return mapper.writeValueAsString(obj);
         } catch (IOException e) {
             LOG.error(e, "Unable to jsonify object of class {}",
                     obj == null ? null : obj.getClass());
@@ -60,18 +78,10 @@ public final class JSON {
         }
     }
 
-    public static String jsonify(final Object obj) {
-        return jsonify(obj, defaultErrorMessage(obj));
-    }
-
     private static String defaultErrorMessage(final Object obj) {
         if (null != obj) {
             return "<? " + obj.getClass().getCanonicalName() + "/>";
         }
         return "<?null />";
-    }
-
-    public static <T> T restoreTyped(final Object source, final Class<T> clazz) throws IOException {
-        return usualMapper.readValue(usualMapper.writeValueAsBytes(source), clazz);
     }
 }
