74384216f9f208662ca377b82e6b9fc8613a9ed0
Ketoth Xupack <ketoth.xupack@gmail.com>
2012-09-22 05:23:40 +0700

    get rid of deprecated Cast utility
    Signed-off-by: Ketoth Xupack <ketoth.xupack@gmail.com>

--- projects/typetools/src/test/java/org/nohope/reflection/CastTest.java
+++ projects/typetools/src/test/java/org/nohope/reflection/CastTest.java
@@ -1,10 +1,10 @@
-package org.nohope.typetools;
+package org.nohope.reflection;
 
 import org.junit.Test;
-import org.nohope.reflection.TypeReference;
 
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertNull;
+import static org.nohope.reflection.IntrospectionUtils.safeCast;
 
 /**
  * Date: 31.07.12
@@ -14,29 +14,28 @@ public class CastTest {
 
     @Test
     public void testCast() {
-        final boolean value1 = Cast.as(true, Boolean.class);
+        final boolean value1 = safeCast(true, Boolean.class);
         assertEquals(true, value1);
 
-        final Boolean value2 = Cast.as(23.0, Boolean.class);
+        final Boolean value2 = safeCast(23.0, Boolean.class);
         assertNull(value2);
     }
 
     @Test(expected = java.lang.NullPointerException.class)
     public void testCastExceptional() {
-        final boolean value2 = Cast.as(23.0, Boolean.class);
+        final boolean value2 = safeCast(23.0, Boolean.class);
         assertNull(value2);
     }
 
     @Test
     public void testNull() {
-        final Boolean value1 = Cast.as(null, Boolean.class);
+        final Boolean value1 = safeCast(null, Boolean.class);
         assertNull(value1);
     }
 
-
     @Test(expected = java.lang.NullPointerException.class)
     public void testCastExceptional2() {
-        final boolean value2 = Cast.as(null, Boolean.class);
+        final boolean value2 = safeCast(null, Boolean.class);
         assertNull(value2);
     }
 
@@ -46,29 +45,28 @@ public class CastTest {
 
     @Test
     public void testCastTR() {
-        final boolean value1 = Cast.as(true, trb);
+        final boolean value1 = safeCast(true, trb);
         assertEquals(true, value1);
 
-        final Boolean value2 = Cast.as(23.0, trb);
+        final Boolean value2 = safeCast(23.0, trb);
         assertNull(value2);
     }
 
     @Test(expected = java.lang.NullPointerException.class)
     public void testCastExceptionalTR() {
-        final boolean value2 = Cast.as(23.0, trb);
+        final boolean value2 = safeCast(23.0, trb);
         assertNull(value2);
     }
 
     @Test
     public void testNullTR() {
-        final Boolean value1 = Cast.as(null, trb);
+        final Boolean value1 = safeCast(null, trb);
         assertNull(value1);
     }
 
-
     @Test(expected = java.lang.NullPointerException.class)
     public void testCastExceptionalTR2() {
-        final boolean value2 = Cast.as(null, trb);
+        final boolean value2 = safeCast(null, trb);
         assertNull(value2);
     }
 }
