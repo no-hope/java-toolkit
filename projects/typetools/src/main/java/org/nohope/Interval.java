fe23de58ae02bd2d37ce0720da6113c3784ee926
Fei Wong Reed <feiwreed@gmail.com>
2012-10-05 11:01:08 +0700

    interval improved

--- projects/typetools/src/main/java/org/nohope/Interval.java
+++ projects/typetools/src/main/java/org/nohope/Interval.java
@@ -4,6 +4,8 @@ import org.joda.time.DateTime;
 import org.joda.time.LocalTime;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 /**
  * Date: 10/4/12
@@ -14,13 +16,42 @@ public final class Interval implements Serializable {
 
     private LocalTime begin;
     private LocalTime end;
+    private final Set<Integer> daysOfWeek = new HashSet<>();
 
-    public Interval() {
+    Interval() {
     }
 
     public Interval(final LocalTime begin, final LocalTime end) {
         this.begin = begin;
         this.end = end;
+    }
+
+    public Interval(final LocalTime begin, final LocalTime end, final Set<Integer> daysOfWeek) {
+        this.begin = begin;
+        this.end = end;
+        setDaysOfWeek(daysOfWeek);
+    }
+
+    public Interval(final LocalTime begin, final LocalTime end, final int day) {
+        this.begin = begin;
+        this.end = end;
+        final Set<Integer> dayOfWeek = new HashSet<>();
+        dayOfWeek.add(day);
+        setDaysOfWeek(dayOfWeek);
+    }
+
+    public Set<Integer> getDaysOfWeek() {
+        return daysOfWeek;
+    }
+
+    /**
+     * The values for the day of week are defined in {@link org.joda.time.DateTimeConstants}.
+     *
+     */
+    public Interval setDaysOfWeek(final Set<Integer> daysOfWeek) {
+        this.daysOfWeek.clear();
+        this.daysOfWeek.addAll(daysOfWeek);
+        return this;
     }
 
     public LocalTime getBegin() {
@@ -42,6 +73,13 @@ public final class Interval implements Serializable {
     }
 
     public boolean contains(final DateTime timestamp) {
+        if (!daysOfWeek.isEmpty()) {
+            final int dayOfWeek = timestamp.getDayOfWeek();
+            if (!daysOfWeek.contains(dayOfWeek)) {
+                return false;
+            }
+        }
+
         final LocalTime time = timestamp.toLocalTime();
         if (begin.isBefore(end)) {
             return begin.isBefore(time) && end.isAfter(time);
