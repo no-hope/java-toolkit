c95d0105b31b5399b6a60d374b136e64bd7fce16
Ketoth Xupack <ketoth.xupack@gmail.com>
2012-10-09 05:36:38 +0700

    TMap cleanup
    Signed-off-by: Ketoth Xupack <ketoth.xupack@gmail.com>

--- projects/typetools/src/main/java/org/nohope/typetools/TMap.java
+++ projects/typetools/src/main/java/org/nohope/typetools/TMap.java
@@ -19,22 +19,11 @@ public class TMap {
         return map.get(key);
     }
 
-    public static <V> V get(final Map<String, V> map, final Enum key) {
-        return map.get(key.toString());
-    }
-
     public static <V, T extends V, K> T safeGet(final Map<K, V> map, final K key, final Class<T> clazz) {
         return safeCast(map.get(key), clazz);
-    }
-    public static <V, T extends V> T safeGet(final Map<String, V> map, final Enum key, final Class<T> clazz) {
-        return safeCast(map.get(key.toString()), clazz);
     }
 
     public static <V, T extends V, K> T safeGet(final Map<K, V> map, final K key, final TypeReference<T> ref) {
         return safeCast(map.get(key), ref);
     }
-    public static <V, T extends V> T safeGet(final Map<String, V> map, final Enum key, final TypeReference<T> ref) {
-        return safeCast(map.get(key.toString()), ref);
-    }
-
 }
