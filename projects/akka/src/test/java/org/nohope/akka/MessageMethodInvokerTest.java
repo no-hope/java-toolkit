c9c83bffa2b4173f0f2805e466ec3497068d1ccf
Ketoth Xupack <ketoth.xupack@gmail.com>
2012-10-23 22:51:37 +0700

    get rid of deprecated junit api
    Signed-off-by: Ketoth Xupack <ketoth.xupack@gmail.com>

--- projects/akka/src/test/java/org/nohope/akka/MessageMethodInvokerTest.java
+++ projects/akka/src/test/java/org/nohope/akka/MessageMethodInvokerTest.java
@@ -2,9 +2,9 @@ package org.nohope.akka;
 
 import org.junit.Test;
 
-import static junit.framework.Assert.assertEquals;
-import static junit.framework.Assert.assertFalse;
-import static junit.framework.Assert.fail;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.fail;
 import static org.junit.Assert.assertTrue;
 import static org.nohope.akka.MessageMethodInvoker.SignaturePair;
 
