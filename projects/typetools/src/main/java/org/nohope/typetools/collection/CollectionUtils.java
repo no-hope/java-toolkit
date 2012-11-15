a3fb879888b001798cf81db264c13092138535ca
Ketoth Xupack <ketoth.xupack@gmail.com>
2012-11-16 02:47:44 +0700

    more helper methods for added to CollectionUtils
    Signed-off-by: Ketoth Xupack <ketoth.xupack@gmail.com>

--- projects/typetools/src/main/java/org/nohope/typetools/collection/CollectionUtils.java
+++ projects/typetools/src/main/java/org/nohope/typetools/collection/CollectionUtils.java
@@ -1,7 +1,11 @@
 package org.nohope.typetools.collection;
 
+import org.nohope.ITranslator;
+
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 
 /**
  * @author <a href="mailto:ketoth.xupack@gmail.com">ketoth xupack</a>
@@ -17,5 +21,43 @@ public final class CollectionUtils {
             placeholders.add(placeholder);
         }
         return placeholders;
+    }
+
+    public static <K, V> Map<K, V> toMap(final Map<K, V> target,
+                                         final Collection<V> collection,
+                                         final ITranslator<V, K> translator) {
+        for (final V value : collection) {
+            target.put(translator.translate(value), value);
+        }
+
+        return target;
+    }
+
+    public static <K, V, C extends Collection<K>> C
+        toCollection(final C target,
+                     final Collection<V> collection,
+                     final ITranslator<V, K> translator) {
+        for (final V value : collection) {
+            target.add(translator.translate(value));
+        }
+
+        return target;
+    }
+
+    public static <K, V, C extends Collection<K>> C
+        toCollection(final C target,
+                     final V[] collection,
+                     final ITranslator<V, K> translator) {
+        for (final V value : collection) {
+            target.add(translator.translate(value));
+        }
+
+        return target;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static <K, V> K[] toArray(final V[] collection,
+                                     final ITranslator<V, K> translator) {
+        return (K[]) toCollection(new ArrayList<K>(), collection, translator).toArray();
     }
 }
