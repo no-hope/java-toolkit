8185cd4ee9130456f7a1c612382fe6b86bac6b9a
Ketoth Xupack <ketoth.xupack@gmail.com>
2013-09-25 00:16:44 +0700

    springtools: small javadoc comment on bean definition
    Signed-off-by: Ketoth Xupack <ketoth.xupack@gmail.com>

--- projects/springtools/src/main/java/org/nohope/spring/BeanDefinition.java
+++ projects/springtools/src/main/java/org/nohope/spring/BeanDefinition.java
@@ -5,8 +5,12 @@ import org.nohope.reflection.TypeReference;
 import javax.annotation.Nonnull;
 
 /**
+ * Class-helper allowing to unify relation between bean name and it's underlying type.
+ *
  * @author <a href="mailto:ketoth.xupack@gmail.com">ketoth xupack</a>
  * @since 10/16/12 7:24 PM
+ * @apiviz.uses org.nohope.reflection.TypeReference
+ * @apiviz.uses java.lang.Class
  */
 public final class BeanDefinition<T> {
     private final String name;
