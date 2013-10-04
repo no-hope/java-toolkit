e2d522bc6911095a55ee64e480ddb89b3543da86
Ketoth Xupack <ketoth.xupack@gmail.com>
2013-10-04 14:22:13 +0700

    validation: more constructors for ValidationException
    Signed-off-by: Ketoth Xupack <ketoth.xupack@gmail.com>

--- projects/validation/src/main/java/org/nohope/validation/ValidationException.java
+++ projects/validation/src/main/java/org/nohope/validation/ValidationException.java
@@ -10,4 +10,12 @@ public class ValidationException extends Exception {
     public ValidationException(final String message) {
         super(message);
     }
+
+    public ValidationException(final String message, final Throwable cause) {
+        super(message, cause);
+    }
+
+    public ValidationException(final Throwable cause) {
+        super(cause);
+    }
 }
