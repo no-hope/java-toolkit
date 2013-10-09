d52ce2ba4539676a504efcc1613b1c94e7247778
Ketoth Xupack <ketoth.xupack@gmail.com>
2013-10-09 17:00:57 +0700

    test-utils: javadocs
    Signed-off-by: Ketoth Xupack <ketoth.xupack@gmail.com>

---
+++ projects/test-utils/src/main/java/org/nohope/test/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Set of test utilities aimed to deal with testing routines.
+ *
+ * @author <a href="mailto:ketoth.xupack@gmail.com">Ketoth Xupack</a>
+ * @since 2013-10-09 16:54
+ * @apiviz.landmark
+ */
+package org.nohope.test;
---
+++ projects/test-utils/src/main/java/org/nohope/test/runner/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Collection of junit runners.
+ *
+ * @author <a href="mailto:ketoth.xupack@gmail.com">Ketoth Xupack</a>
+ * @since 2013-10-09 16:56
+ * @see org.junit.runner.Runner
+ * @apiviz.landmark
+ */
+package org.nohope.test.runner;
