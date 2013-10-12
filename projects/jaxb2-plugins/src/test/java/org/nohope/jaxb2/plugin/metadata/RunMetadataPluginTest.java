baf2f3f9192a28eefcf5f81a2a3de7ce43522c2e
Ketoth Xupack <ketoth.xupack@gmail.com>
2013-10-12 19:12:33 +0700

    jaxb2-plugins: force javac compilation of generated classes
    Signed-off-by: Ketoth Xupack <ketoth.xupack@gmail.com>

--- projects/jaxb2-plugins/src/test/java/org/nohope/jaxb2/plugin/metadata/RunMetadataPluginTest.java
+++ projects/jaxb2-plugins/src/test/java/org/nohope/jaxb2/plugin/metadata/RunMetadataPluginTest.java
@@ -3,9 +3,23 @@ package org.nohope.jaxb2.plugin.metadata;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.jvnet.jaxb2.maven2.AbstractXJC2Mojo;
+import org.nohope.ITranslator;
+import org.nohope.test.ResourceUtils;
+import org.nohope.typetools.collection.CollectionUtils;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 
+import javax.tools.DiagnosticCollector;
+import javax.tools.JavaCompiler;
+import javax.tools.JavaFileObject;
+import javax.tools.SimpleJavaFileObject;
+import javax.tools.ToolProvider;
 import java.io.File;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLClassLoader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import static java.lang.Class.forName;
@@ -25,6 +39,36 @@ public class RunMetadataPluginTest {
     //    System.setProperty("javax.xml.accessExternalDTD", "all");
     //}
 
+    /**
+     * A file object used to represent source coming from a string.
+     */
+    public class JavaSourceFromString extends SimpleJavaFileObject {
+        /**
+         * The source code of this "file".
+         */
+        final String code;
+
+        /**
+         * Constructs a new JavaSourceFromString.
+         * @param name the name of the compilation unit represented by this file object
+         * @param code the source code for the compilation unit represented by this file object
+         */
+        public JavaSourceFromString(final String name,
+                                    final String code) {
+            super(URI.create(
+                    "string:///"
+                    + name.replace('.', '/')
+                    + JavaFileObject.Kind.SOURCE.extension),
+                    JavaFileObject.Kind.SOURCE);
+            this.code = code;
+        }
+
+        @Override
+        public CharSequence getCharContent(final boolean ignoreEncodingErrors) {
+            return code;
+        }
+    }
+
     private static class RunMetadataPlugin extends org.jvnet.jaxb2.maven2.test.RunXJC2Mojo {
 
         @Override
@@ -33,9 +77,15 @@ public class RunMetadataPluginTest {
         }
 
         @Override
+        protected File getGeneratedDirectory() {
+            return new File(getBaseDir(), "target/test-classes");
+        }
+
+        @Override
         protected void configureMojo(final AbstractXJC2Mojo mojo) {
             super.configureMojo(mojo);
             mojo.setForceRegenerate(true);
+            mojo.setAddCompileSourceRoot(true);
         }
 
         @Override
@@ -53,6 +103,57 @@ public class RunMetadataPluginTest {
 
     @Test
     public void checkGeneratedObjectsValidity() throws Exception {
+        final JavaCompiler compiler = ToolProvider.getSystemJavaCompiler();
+        final DiagnosticCollector<JavaFileObject> diagnostics = new DiagnosticCollector<>();
+
+        final String root = getClass().getProtectionDomain()
+                .getCodeSource()
+                .getLocation()
+                .getFile();
+
+        final PathMatchingResourcePatternResolver resolver =
+                new PathMatchingResourcePatternResolver();
+
+        final List<JavaSourceFromString> units = new ArrayList<>();
+        final List<String> classFiles = new ArrayList<>();
+        final List<String> classes = new ArrayList<>();
+        for (final Resource resource : resolver.getResources("classpath*:org/nohope/metadata/**/*.java")) {
+            final String file = resource.getFile().getAbsolutePath().replace(root, "");
+            if (file.endsWith(".java")) {
+                final String prefix = file.substring(0, file.length() - 5);
+                final String code = ResourceUtils.getResourceAsString("/" + file);
+                assertNotNull(code);
+                final String fqdn = prefix.replace("/", ".");
+                units.add(new JavaSourceFromString(fqdn, code));
+                classFiles.add("/" + prefix + ".class");
+                if (!prefix.endsWith("package-info")) {
+                    classes.add(fqdn);
+                }
+            }
+        }
+
+        final JavaCompiler.CompilationTask task =
+                compiler.getTask(null, null, diagnostics, Arrays.asList("-d", root), null, units);
+
+        if (!task.call()) {
+            throw new IllegalStateException();
+        }
+
+        final URLClassLoader ucl = new URLClassLoader(
+                CollectionUtils.mapArray(classFiles.toArray(new String[classFiles.size()]), URL.class,
+                        new ITranslator<String, URL>() {
+                            @Override
+                            public URL translate(final String source) {
+                                final URL resource = ResourceUtils.getResource(source);
+                                assertNotNull(resource);
+                                return resource;
+                            }
+                        }));
+
+        for (final String fqdn : classes) {
+            ucl.loadClass(fqdn);
+        }
+
         final Class<?> cobj1 = forName("org.nohope.metadata.ComplexObjectType");
         final Object descriptor = invoke(cobj1, ALL, "descriptor");
 
