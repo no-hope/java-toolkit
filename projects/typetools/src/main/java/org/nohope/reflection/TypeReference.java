97234688f52d3f043d27f036dfb76491b64eca5a
Ketoth Xupack <ketoth.xupack@gmail.com>
2013-10-14 18:18:15 +0700

    typetools: type referense: equals, hashCode, toString and erasure helper
    Signed-off-by: Ketoth Xupack <ketoth.xupack@gmail.com>

--- projects/typetools/src/main/java/org/nohope/reflection/TypeReference.java
+++ projects/typetools/src/main/java/org/nohope/reflection/TypeReference.java
@@ -2,8 +2,11 @@ package org.nohope.reflection;
 
 import javax.annotation.Nonnull;
 import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.lang.reflect.ParameterizedType;
 import java.lang.reflect.Type;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * References a generic type.
@@ -16,13 +19,10 @@ public abstract class TypeReference<T> {
 
     /** Type of token. */
     private final Type type;
-
     /** Class of type parameter. */
     private final Class<T> rawType;
 
-    /**
-     * Default constructor.
-     */
+    /** Default constructor. */
     @SuppressWarnings("unchecked")
     public TypeReference() {
         if (!getClass().isAnonymousClass()) {
@@ -47,35 +47,124 @@ public abstract class TypeReference<T> {
     }
 
     /**
+     * 'Erasured' variant of type reference. Useful for wrapping {@link Class Class} instances.
+     * <p/>
+     * <p/>
+     * Example
+     * <pre>
+     *    final TypeReference<Map> ref = TypeReference.erasure(Map.class);
+     * </pre>
+     *
+     * @param clazz class to be wrapped
+     */
+    private TypeReference(final Class<T> clazz) {
+        this.type = clazz;
+        this.rawType = clazz;
+    }
+
+    private static String repr(final Type type) {
+        if (!(type instanceof Class)) {
+            return type.toString();
+        }
+        final Class clazz = (Class) type;
+        if (clazz.isArray()) {
+            Class component = (Class) type;
+            String postfix = "";
+            while (component.isArray()) {
+                component = component.getComponentType();
+                postfix += "[]";
+            }
+            return repr(component) + postfix;
+        }
+
+        if (clazz.isLocalClass()) {
+            final Pattern localOrAnonymous = Pattern.compile("^.*\\$\\d([^\\$\\d]+)$");
+            final Matcher matcher = localOrAnonymous.matcher(clazz.getName());
+            final String name = matcher.matches() ? matcher.group(1) : clazz.getName();
+            final Method method = clazz.getEnclosingMethod();
+            final String methodName = method == null ? "new" : method.getName();
+            return "{"
+                   + repr(clazz.getEnclosingClass())
+                   + "#"
+                   + methodName
+                   + " -> "
+                   + name
+                   + "}";
+        }
+        if (clazz.isAnonymousClass()) {
+            final Method method = clazz.getEnclosingMethod();
+            final String methodName = method == null ? "new" : method.getName();
+            return "{"
+                   + repr(clazz.getEnclosingClass())
+                   + "#"
+                   + methodName
+                   + " -> <anonymous>"
+                   + "}";
+        }
+
+        return clazz.getCanonicalName();
+    }
+
+    public static <T> TypeReference<T> erasure(final Class<T> type) {
+        return new ErasureTypeReference<>(type);
+    }
+
+    /**
      * Instantiates a new instance of {@code T} using compatible constructor.
      *
      * @param args constructor arguments for referenced type.
      * @return new instance of type token
-     * @throws IllegalAccessException    on reflection error
-     * @throws InstantiationException    on reflection error
-     * @throws NoSuchMethodException     on reflection error
+     *
+     * @throws IllegalAccessException on reflection error
+     * @throws InstantiationException on reflection error
+     * @throws NoSuchMethodException on reflection error
      * @throws InvocationTargetException on reflection error
      * @see IntrospectionUtils#newInstance(Class, Object[])
      */
     public final T newInstance(final Object... args)
             throws NoSuchMethodException, IllegalAccessException,
-            InvocationTargetException, InstantiationException {
+                   InvocationTargetException, InstantiationException {
         return IntrospectionUtils.newInstance(getTypeClass(), args);
     }
 
-    /**
-     * @return class of referenced type
-     */
+    /** @return class of referenced type */
     @Nonnull
     public final Class<T> getTypeClass() {
         return rawType;
     }
 
-    /**
-     * @return the referenced type
-     */
+    /** @return the referenced type */
     @Nonnull
     public final Type getType() {
         return this.type;
+    }
+
+    @Override
+    public final int hashCode() {
+        return 31 * type.hashCode() + rawType.hashCode();
+    }
+
+    @Override
+    public final boolean equals(final Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (!(o instanceof TypeReference)) {
+            return false;
+        }
+
+        final TypeReference that = (TypeReference) o;
+        return rawType.equals(that.rawType) && type.equals(that.type);
+    }
+
+    @Override
+    public final String toString() {
+        return "TypeReference<" + repr(type) + ">";
+    }
+
+    private static final class ErasureTypeReference<T> extends TypeReference<T> {
+        private ErasureTypeReference(final Class<T> type) {
+            super(type);
+        }
     }
 }
--- projects/typetools/src/test/java/org/nohope/reflection/TypeReferenceTest.java
+++ projects/typetools/src/test/java/org/nohope/reflection/TypeReferenceTest.java
@@ -4,7 +4,13 @@ import org.junit.Test;
 
 import java.net.InetSocketAddress;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
 
+import static com.mongodb.util.MyAsserts.assertFalse;
+import static com.mongodb.util.MyAsserts.assertNotEquals;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
@@ -51,14 +57,11 @@ public final class TypeReferenceTest {
     @Test
     public void simpleTest() {
         assertEquals(Integer[].class,
-                new TypeReference<Integer[]>() {
-                }.getTypeClass());
+                new TypeReference<Integer[]>() {}.getTypeClass());
         assertEquals((new ArrayList<Integer>()).getClass(),
-                new TypeReference<ArrayList<Integer>>() {
-                }.getTypeClass());
+                new TypeReference<ArrayList<Integer>>() {}.getTypeClass());
         assertEquals(Integer[][].class,
-                new TypeReference<Integer[][]>() {
-                }.getTypeClass());
+                new TypeReference<Integer[][]>() {}.getTypeClass());
     }
 
     @Test
@@ -71,6 +74,56 @@ public final class TypeReferenceTest {
 
         assertEquals(123, address.getPort());
         assertEquals(InetSocketAddress.class, addressTypeReference.getType());
+    }
+
+    @Test
+    @SuppressWarnings({
+            "ObjectEqualsNull",
+            "LiteralAsArgToStringEquals",
+            "EqualsBetweenInconvertibleTypes",
+            "AssertEqualsBetweenInconvertibleTypes"})
+    public void equality() throws Exception {
+        assertEquals(new TypeReference<List<Integer>>() {},
+                new TypeReference<List<Integer>>() {});
+        assertNotEquals(new TypeReference<List<Integer>>() {},
+                new TypeReference<List<Boolean>>() {});
+
+        assertNotEquals(new TypeReference<List<Integer>>() {},
+                new TypeReference<List>() {});
+
+        assertFalse(new TypeReference<List<Integer>>() {
+        }.equals(null));
+        assertFalse(new TypeReference<List<Integer>>() {}.equals(""));
+        final TypeReference<List<Integer>> ref = new TypeReference<List<Integer>>() {};
+        final TypeReference<List<Integer>> ref2 = new TypeReference<List<Integer>>() {};
+        assertEquals(ref, ref);
+        assertEquals(ref.hashCode(), ref2.hashCode());
+    }
+
+    @Test
+    public void erasure() throws Exception {
+        final TypeReference<Map> ref = TypeReference.erasure(Map.class);
+        final TypeReference<Map> ref2 = new TypeReference<Map>() {};
+        assertEquals(ref, ref2);
+
+        final Map<String, String> map1 = new HashMap<>();
+        final Map<String, String> map2 = new LinkedHashMap<>();
+        final TypeReference<? extends Map> erasure1 = TypeReference.erasure(map1.getClass());
+        final TypeReference<? extends Map> erasure2 = TypeReference.erasure(map2.getClass());
+        assertNotEquals(erasure1, erasure2);
+    }
+
+    @Test
+    public void repr() throws Exception {
+        class TC {}
+        assertEquals("TypeReference<java.util.List<java.lang.Integer>>",
+                new TypeReference<List<Integer>>() {}.toString());
+        assertEquals("TypeReference<int[][]>",
+                new TypeReference<int[][]>() {}.toString());
+        assertEquals("TypeReference<{org.nohope.reflection.TypeReferenceTest#repr -> TC}>",
+                TypeReference.erasure(TC.class).toString());
+        assertEquals("TypeReference<{org.nohope.reflection.TypeReferenceTest#repr -> <anonymous>}>",
+                TypeReference.erasure(new TC() {}.getClass()).toString());
     }
 
     private abstract static class ReferenceHolderAbstractChild<T>
