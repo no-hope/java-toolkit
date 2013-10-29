cdfcba619f8f2377195bec69aaece3f6ee357aa1
Ketoth Xupack <ketoth.xupack@gmail.com>
2013-10-29 16:49:12 +0700

    travis: scripts optimizations
    Signed-off-by: Ketoth Xupack <ketoth.xupack@gmail.com>

---
+++ scripts/deploy.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+TRAVIS_PULL_REQUEST=${TRAVIS_PULL_REQUEST-true}
+if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
+    mvn -q -e -s ~/.m2/settings_deploy.xml -Dskip.coveralls=true -Dskip.tests=true deploy
+    # TODO: uncomment when site will be ready
+    # mvn -q -e -s ~/.m2/settings_deploy.xml -Dskip.coveralls=true -Dskip.tests=true site-deploy
+fi
---
+++ scripts/prepare_settings.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+SONATYPE_PASSWORD=${SONATYPE_PASSWORD-}
+GITHUB_PASSWORD=${GITHUB_PASSWORD-}
+GITHUB_PASSWORD="$(echo ${GITHUB_PASSWORD} | base64 -d)"
+
+echo "
+<settings>
+    <servers>
+        <server>
+            <id>sonatype-nexus-snapshots</id>
+            <username>ketoth.xupack</username>
+            <password>${SONATYPE_PASSWORD}</password>
+        </server>
+        <server>
+            <id>github</id>
+            <username>ketoth.xupack@gmail.com</username>
+            <password>${GITHUB_PASSWORD}</password>
+        </server>
+    </servers>
+</settings>
+" > ~/.m2/settings_deploy.xml
