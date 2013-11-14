6a9e1141047a409ba064fa97f08ba4a2e14f1779
Ketoth Xupack <ketoth.xupack@gmail.com>
2013-11-14 15:07:02 +0700

    deploy script fix
    Signed-off-by: Ketoth Xupack <ketoth.xupack@gmail.com>

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-TRAVIS_PULL_REQUEST=${TRAVIS_PULL_REQUEST-true}
+#!/bin/bash
+TRAVIS_PULL_REQUEST=${TRAVIS_PULL_REQUEST-false}
 if [[ "${TRAVIS_PULL_REQUEST}" == "false" ]]; then
     mvn -q -e -s ~/.m2/settings_deploy.xml -Dskip.coveralls=true -Dskip.tests=true deploy
     # TODO: uncomment when site will be ready
