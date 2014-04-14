223fde90adf5c2e9edd8e7faec640f9e4588b823
Ketoth Xupack <ketoth.xupack@gmail.com>
2014-04-14 17:51:17 +0700

    akka: akka-parent module introduced
    Signed-off-by: Ketoth Xupack <ketoth.xupack@gmail.com>

--- projects/akka/akka-messaging/src/main/java/org/nohope/akka/invoke/Signature.java
+++ projects/akka/akka-messaging/src/main/java/org/nohope/akka/invoke/Signature.java
@@ -20,7 +20,7 @@ final class Signature {
     }
 
     @Override
-    public boolean equals(Object o) {
+    public boolean equals(final Object o) {
         if (this == o) {
             return true;
         }
